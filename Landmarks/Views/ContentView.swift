804e1e54f8031f18dd5ad5019b79481aee234fdc
= <samgusaapp1993@gmail.com>
2021-06-22 21:45:13 -0400

    Accidentally damaged the content view, so had to recreate it, not committing it to main.

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Sam Greenhill on 6/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
