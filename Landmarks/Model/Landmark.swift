ab331b78de7151e5b4658d557d9f2cc2262ac29e
= <samgusaapp1993@gmail.com>
2021-06-28 22:24:40 -0400

    Adding UIKit to SwiftUI. Create a View to represent a UIPageViewController. Create the View Controller's Data Source.

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -33,6 +33,9 @@ struct Landmark: Hashable, Codable, Identifiable {
         Image(imageName)
     }
     
+    var featureImage: Image? {
+        isFeatured ? Image(imageName + "_feature") : nil
+    }
     
     private var coordinates: Coordinates
     
---
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -0,0 +1,66 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Sam Greenhill on 6/28/21.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    
+    var pages: [Page]
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    //creates a UIPageViewController with desired configurations
+    //swiftUI calls this method a single time when it's ready to display the view, and then manages the view controller's life cycle.
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
+    }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return controllers.last
+            }
+            return controllers[index - 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            return controllers[index + 1]
+        }
+        
+    }
+    
+}
